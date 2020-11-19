ae2f5ff83f540bd031ba3987930088046e71dafd
Amanda D'Argenio <dargenioa14@gmail.com>
2020-11-18 22:44:22 -0500

    posting and getting notes on backend server through Postman

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,28 +1,33 @@
 const fs = require("fs");
-const dbJSON = require("../db/db.json")
+const dbJSON = require("../db/db.json");
 
 //https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/500
 
 module.exports = (app) => {
 
     // //read JSON file
-        app.get("/api/notes", function (req, res) {
-            res.json(dbJSON);
+    app.get("/api/notes", function (req, res) {
+        res.json(dbJSON);
+
+    });
+
+    app.post("/api/notes", function (req, res) {
+
+        let newNote = req.body;
+        dbJSON.push(newNote);
+        //console.log(dbJSON);
+        //let jsonString = JSON.stringify(dbJSON);
+        res.json({ ok: true });
 
 
+        // fs.writeFile("../db/testdb.json", jsonString, (error, data) => {
+        //     error ? console.error(error) : console.log(JSON.parse(data))
 
-        });
-
-        app.post("/api/notes", function (req, res) {
-
-            let newNote = req.body;
-            dbJSON.push(JSON.parse(newNote))
-            console.log(dbJSON);
-
-        });
+    });
 
 
 };
+
 
     // app.post("/api/notes/:id", function (req, res) {
     //     // still working on
