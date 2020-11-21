692b1cd00aafd1512d28e8209bd55fa7ff5a6e08
Amanda D'Argenio <dargenioa14@gmail.com>
2020-11-21 10:12:46 -0500

    fixed uuid to produce random number

--- routes/api-route.js
+++ routes/api-route.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const router = require("express").Router();
 const dbJSON = require("../db/db.json");
 const { v4: uuidv4 } = require('uuid');
-const id = uuidv4();
 
 // //read JSON file
 router.get("/api/notes", function (req, res) {
@@ -22,6 +21,7 @@ router.post("/api/notes", function (req, res) {
 
     fs.readFile("./db/db.json", "utf8", (error, data) => {
         data = JSON.parse(data);
+        const id = uuidv4();
         let newNote = { id, ...req.body };
         let allNotes = [...data, newNote];
         let jsonString = JSON.stringify(allNotes);
@@ -53,93 +53,7 @@ router.delete("/api/notes/:id", (req, res) => {
 
 });
 
-// dbJSON.forEach(num => {
-//     if (req.params.id == num.id) {
-//         console.log(req.params);
-//         dbJSON.splice()
-//         let deletedFile = dbJSON.slice();
-//         let newJSON = JSON.stringify(deletedFile)
-
-//         fs.writeFile("./db/testdb.json", newJSON, function (err) {
-//             if (err) {
-//                 return console.log(err);
-//             }
-//             console.log("Success!");
-//         })
-//     };
-
-// });
-
-//     });
-
-
-// router.delete("/api/notes/:id", (req, res) => {
-
-//     dbJSON.forEach((num, index) => {
-//         if (req.params.id == num.id) {
-//             dbJSON.splice(index, 1)
-//             let deletedFile = dbJSON.slice();
-//             let newJSON = JSON.stringify(deletedFile)
-
-//             fs.writeFileSync("./db/db.json", newJSON, function (err) {
-//                 if (err) {
-//                     return console.log(err);
-//                 }
-//                 console.log("Success!");
-//             })
-//         };
-
-//     });
-// });
-
-
-// router.get("api/notes/:id", function (req, res) {
-
-
-
-//     // router.delete("api/notes/:id" + id, function (req, res) {
-//     //     console.log(dataDelete.id);
-
-//     // });
-
-
-// });
-
 
 
 module.exports = router;
-
-
-//module.exports = (app) => {
-
-    // // //read JSON file
-    // app.get("/notes", function (req, res) {
-    //     res.json(dbJSON);
-
-    // });
-
-    // app.post("/notes", function (req, res) {
-
-    //     let newNote = req.body;
-    //     dbJSON.push(newNote);
-    //     console.log(dbJSON);
-    //     res.json({ ok: true });
-
-    //     //Do we need write file?
-
-    //     // let jsonString = JSON.stringify(dbJSON);
-
-    //     // fs.writeFile("../db/db.json", jsonString, (error, data) => {
-    //     //     error ? console.error(error) : console.log(JSON.parse(data))
-
-    // });
-
-        // app.post("/api/notes/:id", function (req, res) {
-    //     // still working on
-
-    //     res.json({ ok: true });
-    // });
-
-
-
 
